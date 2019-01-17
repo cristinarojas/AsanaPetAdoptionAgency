0697d60b40596196edf3d0580280e0af41de9355
Cristina Rojas <telematicacristina@gmail.com>
2019-01-17 15:55:52 -0800

    Adding code to load pets info from array

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -4,15 +4,49 @@ import React, { Fragment } from 'react';
 // Styles
 import styles from './Gallery.scss';
 
-// Images
-import p1 from '@images/pets/1.jpeg';
-import p2 from '@images/pets/2.jpeg';
-import p3 from '@images/pets/3.jpeg';
-import p4 from '@images/pets/4.jpeg';
-import p5 from '@images/pets/5.jpeg';
-import p6 from '@images/pets/6.jpeg';
-import p7 from '@images/pets/7.jpeg';
-import p8 from '@images/pets/8.jpeg';
+// Data
+const dogs = [
+  {
+    "image": "pets/1.jpeg",
+    "source": "https://unsplash.com/photos/ybHtKz5He9Y",
+    "name": "bonnie"
+  },
+  {
+    "image": "pets/2.jpeg",
+    "source": "https://unsplash.com/photos/rY79kwEEyGo",
+    "name": "toby"
+  },
+  {
+    "image": "pets/3.jpeg",
+    "source": "https://unsplash.com/photos/ngqyo2AYYnE",
+    "name": "missy"
+  },
+  {
+    "image": "pets/4.jpeg",
+    "source": "https://unsplash.com/photos/9A64wd1QaAU",
+    "name": "buster"
+  },
+  {
+    "image": "pets/5.jpeg",
+    "source": "https://unsplash.com/photos/915UJQaxtrk",
+    "name": "lucky"
+  },
+  {
+    "image": "pets/6.jpeg",
+    "source": "https://unsplash.com/photos/mx0DEnfYxic",
+    "name": "bear"
+  },
+  {
+    "image": "pets/7.jpeg",
+    "source": "https://unsplash.com/photos/OsOQhAzcEKc",
+    "name": "milo"
+  },
+  {
+    "image": "pets/8.jpeg",
+    "source": "https://unsplash.com/photos/6WneSv56YVI",
+    "name": "honey"
+  },
+];
 
 const Gallery = () => {
   return (
@@ -27,82 +61,17 @@ const Gallery = () => {
 
           <div className={styles.scroll}>
             <ul className={styles.list}>
-              <li>
-                <a href="#">
-                  <img className={styles.min} src={p1} alt="Asana" />
-                  <div className={styles.show}>
-                    <img className={styles.max} src={p1} alt="Asana" />
-                  </div>
-                </a>
-              </li>
-
-              <li>
-                <a href="#">
-                  <img className={styles.min} src={p2} alt="Asana" />
-                  <div className={styles.show}>
-                    <img className={styles.max} src={p2} alt="Asana" />
-                  </div>
-                </a>
-              </li>
-
-              <li>
-                <a href="#">
-                  <img className={styles.min} src={p3} alt="Asana" />
-                  <div className={styles.show}>
-                    <img className={styles.max} src={p3} alt="Asana" />
-                  </div>
-                </a>
-              </li>
-
-
-              <li>
-                <a href="#">
-                  <img className={styles.min} src={p4} alt="Asana" />
-                  <div className={styles.show}>
-                    <img className={styles.max} src={p4} alt="Asana" />
-                  </div>
-                </a>
-              </li>
-
-
-              <li>
-                <a href="#">
-                  <img className={styles.min} src={p5} alt="Asana" />
-                  <div className={styles.show}>
-                    <img className={styles.max} src={p5} alt="Asana" />
-                  </div>
-                </a>
-              </li>
-
-
-              <li>
-                <a href="#">
-                  <img className={styles.min} src={p6} alt="Asana" />
-                  <div className={styles.show}>
-                    <img className={styles.max} src={p6} alt="Asana" />
-                  </div>
-                </a>
-              </li>
-
-
-              <li>
-                <a href="#">
-                  <img className={styles.min} src={p7} alt="Asana" />
-                  <div className={styles.show}>
-                    <img className={styles.max} src={p7} alt="Asana" />
-                  </div>
-                </a>
-              </li>
-
-
-              <li>
-                <a href="#">
-                  <img className={styles.min} src={p8} alt="Asana" />
-                  <div className={styles.show}>
-                    <img className={styles.max} src={p8} alt="Asana" />
-                  </div>
-                </a>
-              </li>
+              { dogs.map((dog, key) => (
+                <li key={key}>
+                  <a href="#">
+                    <p>{dog.name}</p>
+                    <img className={styles.min} src={require(`./${dog.image}`)} alt="Asana" />
+                    <div className={styles.show}>
+                      <img className={styles.max} src={require(`./${dog.image}`)} alt="Asana" />
+                    </div>
+                  </a>
+                </li>
+              ))}
             </ul>
           </div>
         </div>
