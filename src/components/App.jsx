6a44d0782a6e7539ef5536fd8386f34d6f236b7b
Cristina Rojas <telematicacristina@gmail.com>
2019-01-19 19:29:04 -0800

    Adding Helmet for SEO

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,11 @@
 // Dependencies
 import React from 'react';
+import Helmet from 'react-helmet';
 
 // Shared Components
-import Header from './shared/Header/Header';
-import Footer from './shared/Footer/Footer';
-import Text from './shared/Content/Text/Text';
-
+import Header from '@layout/Header/Header';
+import Footer from '@layout/Footer/Footer';
+import Text from '@layout/Content/Text/Text';
 
 // Components
 import Gallery from './Gallery/Gallery';
@@ -14,6 +14,19 @@ import Puppies from './Puppies/Puppies';
 // Main layout
 const App = props => (
   <div>
+    <Helmet
+      title="Person Information"
+      meta={[
+        {
+          name: 'title',
+          content: 'Person Information'
+        },
+        {
+          name: 'description',
+          content: 'This recipe talks about React Helmet'
+        }
+      ]}
+    />
     <Header />
     <Puppies />
     <Text />
