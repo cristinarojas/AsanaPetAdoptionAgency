1d21c2f3f89a33f376a44b0a8d465fe69aaa96eb
Cristina Rojas <telematicacristina@gmail.com>
2019-01-19 19:26:58 -0800

    Adding data

---
+++ src/shared/data/shelter15Pets.js
@@ -0,0 +1,78 @@
+// Data 15
+export default [
+  {
+    "image": "pets/1.jpg",
+    "source": "https://unsplash.com/photos/ybHtKz5He9Y",
+    "name": "bonnie"
+  },
+  {
+    "image": "pets/2.jpg",
+    "source": "https://unsplash.com/photos/rY79kwEEyGo",
+    "name": "toby"
+  },
+  {
+    "image": "pets/3.jpg",
+    "source": "https://unsplash.com/photos/ngqyo2AYYnE",
+    "name": "missy"
+  },
+  {
+    "image": "pets/4.jpg",
+    "source": "https://unsplash.com/photos/9A64wd1QaAU",
+    "name": "buster"
+  },
+  {
+    "image": "pets/5.jpg",
+    "source": "https://unsplash.com/photos/915UJQaxtrk",
+    "name": "lucky"
+  },
+  {
+    "image": "pets/6.jpg",
+    "source": "https://unsplash.com/photos/mx0DEnfYxic",
+    "name": "bear"
+  },
+  {
+    "image": "pets/7.jpg",
+    "source": "https://unsplash.com/photos/OsOQhAzcEKc",
+    "name": "milo"
+  },
+  {
+    "image": "pets/8.jpg",
+    "source": "https://unsplash.com/photos/6WneSv56YVI",
+    "name": "honey"
+  },
+  {
+    "image": "pets/9.jpg",
+    "source": "https://unsplash.com/photos/CdVG9f96kyg",
+    "name": "honey"
+  },
+  {
+    "image": "pets/10.jpg",
+    "source": "https://unsplash.com/photos/bVA2DI7c9e8",
+    "name": "honey"
+  },
+  {
+    "image": "pets/11.jpg",
+    "source": "https://unsplash.com/photos/GvhSUEN-Lm8",
+    "name": "honey"
+  },
+  {
+    "image": "pets/12.jpg",
+    "source": "https://unsplash.com/photos/t2XHKIdc4Kw",
+    "name": "honey"
+  },
+  {
+    "image": "pets/13.jpg",
+    "source": "https://unsplash.com/photos/saRU3vzmgkY",
+    "name": "honey"
+  },
+  {
+    "image": "pets/14.jpg",
+    "source": "https://unsplash.com/photos/1z-kI6gIEpg",
+    "name": "honey"
+  },
+  {
+    "image": "pets/15.jpg",
+    "source": "https://unsplash.com/photos/zHZ2nTnBnAQ",
+    "name": "honey"
+  }
+];
