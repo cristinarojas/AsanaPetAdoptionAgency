4404ebb50b03d97bcf3751e01831f028535554c6
Cristina Rojas <telematicacristina@gmail.com>
2019-01-19 19:31:21 -0800

    Refector my gallery code

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -5,91 +5,8 @@ import LazyLoad from 'react-lazy-load';
 // Styles
 import styles from './Gallery.scss';
 
-// Data
-const dogs = [
-  {
-    "image": "pets/1.jpeg",
-    "source": "https://unsplash.com/photos/ybHtKz5He9Y",
-    "name": "bonnie"
-  },
-  {
-    "image": "pets/2.jpeg",
-    "source": "https://unsplash.com/photos/rY79kwEEyGo",
-    "name": "toby"
-  },
-  {
-    "image": "pets/3.jpeg",
-    "source": "https://unsplash.com/photos/ngqyo2AYYnE",
-    "name": "missy"
-  },
-  {
-    "image": "pets/4.jpeg",
-    "source": "https://unsplash.com/photos/9A64wd1QaAU",
-    "name": "buster"
-  },
-  {
-    "image": "pets/5.jpeg",
-    "source": "https://unsplash.com/photos/915UJQaxtrk",
-    "name": "lucky"
-  },
-  {
-    "image": "pets/6.jpeg",
-    "source": "https://unsplash.com/photos/mx0DEnfYxic",
-    "name": "bear"
-  },
-  {
-    "image": "pets/7.jpeg",
-    "source": "https://unsplash.com/photos/OsOQhAzcEKc",
-    "name": "milo"
-  },
-  {
-    "image": "pets/8.jpeg",
-    "source": "https://unsplash.com/photos/6WneSv56YVI",
-    "name": "honey"
-  },
-  ,
-  {
-    "image": "pets/9.jpeg",
-    "source": "https://unsplash.com/photos/CdVG9f96kyg",
-    "name": "honey"
-  },
-  ,
-  {
-    "image": "pets/10.jpeg",
-    "source": "https://unsplash.com/photos/bVA2DI7c9e8",
-    "name": "honey"
-  },
-  ,
-  {
-    "image": "pets/11.jpeg",
-    "source": "https://unsplash.com/photos/GvhSUEN-Lm8",
-    "name": "honey"
-  },
-  ,
-  {
-    "image": "pets/12.jpeg",
-    "source": "https://unsplash.com/photos/t2XHKIdc4Kw",
-    "name": "honey"
-  },
-  ,
-  {
-    "image": "pets/13.jpeg",
-    "source": "https://unsplash.com/photos/saRU3vzmgkY",
-    "name": "honey"
-  },
-  ,
-  {
-    "image": "pets/14.jpeg",
-    "source": "https://unsplash.com/photos/1z-kI6gIEpg",
-    "name": "honey"
-  },
-  ,
-  {
-    "image": "pets/15.jpeg",
-    "source": "https://unsplash.com/photos/zHZ2nTnBnAQ",
-    "name": "honey"
-  }
-];
+// Data.
+import dogs from '@data/shelter2000Pets.js';
 
 class Gallery extends Component {
   constructor() {
@@ -97,24 +14,25 @@ class Gallery extends Component {
 
     // Handling Show, hide & showing selected image.
     this.state = {
-      toggle: true,
-      selectedDog: '',
-      close: false
+      toggle: false,
+      selectedDog: ''
     };
   }
 
-  // Method - Show, hide & obtaining selected image.
-  showImage(dog) {
-    this.setState({
-      toggle: !this.state.toggle, // To apply a class and show big image
-      selectedDog: dog.image // To print the path of the big image (selected)
-    });
+  updateBodyStyles() {
+    if (this.state.toggle) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = 'visible';
+    }
   }
 
-  close() {
-    this.setState({
-      close: !this.state.close // To apply the class and hide big image
-    });
+  // Method - Show, hide & obtaining selected image.
+  toggleImage(dog = false) {
+    this.setState(() => ({
+      toggle: !this.state.toggle,
+      selectedDog: dog ? dog.image : ''
+    }), this.updateBodyStyles);
   }
 
   render() {
@@ -132,7 +50,6 @@ class Gallery extends Component {
             <div className={styles.scroll}>
               <ul className={styles.list}>
                 { dogs.map((dog, key) => {
-
                   const selectedImage = this.state.selectedDog
                     ? <img className={styles.image} src={require(`./${this.state.selectedDog}`)} alt="Asana" />
                     : '';
@@ -140,29 +57,35 @@ class Gallery extends Component {
                   return (
                     <li
                       key={key}
-                      onClick={(() => this.showImage(dog))}
-                      >
-                        <LazyLoad height={100} offsetVertical={300}>
+                      onClick={(() => this.toggleImage(dog))}
+                    >
+                      <LazyLoad height={100} offsetVertical={800}>
+                        <>
                           <img className={styles.thumbnailImage} src={require(`./${dog.image}`)} alt="Asana" />
-                        </LazyLoad>
-                        <span className={styles.name}>{dog.name}</span>
+                          <span className={styles.name}>{dog.name}</span>
 
-                        <div className={`${this.state.toggle ? styles.hide : styles.show }`}>
-                          <div className={styles.selectedContainer}>
-                            <span
-                              className={styles.close}
-                              onClick={(() => this.close())}
-                            >x</span>
-                            {selectedImage}
+                          <div className={`${!this.state.toggle ? styles.hide : styles.show }`}>
+                            <div className={styles.selectedContainer}>
+                              <span
+                                className={styles.close}
+                                onClick={(() => this.toggleImage())}
+                              >
+                                x
+                              </span>
+                              {selectedImage}
+                            </div>
                           </div>
-                        </div>
+                        </>
+                      </LazyLoad>
+
+
                     </li>
                   );
                 })}
               </ul>
             </div>
           </aside>
-      	</section>
+        </section>
       </Fragment>
     );
   }
